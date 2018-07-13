40fba6bef8440cfbfb5aa827b7907a5d5a863f50
Jose Barron Tovar <josebarrontovar@hotmail.com>
2018-07-13 08:44:05 -0500

    List

--- WebFinal/WebFinal/Controllers/ProjectController.cs
+++ WebFinal/WebFinal/Controllers/ProjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebFinal.ViewModels;
 
 namespace WebFinal.Controllers
 {
@@ -11,7 +12,8 @@ namespace WebFinal.Controllers
         // GET: Project
         public ActionResult Index()
         {
-            return View();
+            FormsViewModel forms = new FormsViewModel();
+            return View(forms);
         }
     }
 }
